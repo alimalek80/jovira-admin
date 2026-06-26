aa29dacb9ce32d3f6ea371871bf78bb94f602958
Ali Malek <alianta2016@gmail.com>
2026-06-27 01:55:28 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    "192.168.1.16",
-    "192.168.1.26",
-    "192.168.1.30",
-    "192.168.1.187",
-    "172.29.224.1",
+    "192.168.1.*",
+    "172.29.*.*",
   ],
 };
 
